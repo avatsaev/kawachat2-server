676c05ef69cc804a11c141651619eae5aafa55b6
A. Vatsaev <a.vatsaev@gmail.com>
2017-06-10 17:00:03 +0200

    add stats to the server

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,6 @@ app.set('port', port);
 app.set('env', env);
 
 
-
-
 const router = express.Router();              // get an instance of the express Router
 
 
@@ -23,10 +21,20 @@ router.get('/', (req, res) => {
 });
 
 router.get('/stats', (req, res) => {
-  res.json({
-    clientsCount: io.engine.clientsCount,
-    roomsCount: Object.keys(io.sockets.adapter.rooms).length,
-  });
+  res.json(getServerStats());
+});
+
+router.get('/room/:frq/stats', (req, res) => {
+
+  if(io.sockets.adapter.rooms[req.params.frq]){
+    res.json({
+      frq: req.params.frq,
+      clientsCount: io.sockets.adapter.rooms[req.params.frq].length
+    });
+  }else{
+    res.status(404);
+  }
+
 });
 
 
@@ -39,46 +47,86 @@ const server = app.listen(app.get('port'), () => {
 const io = socketio(server, {'origins': '*:*'} );
 
 
-const escape_html = function(text = ""): string {
+const escapeHtml = (text = ""): string => {
   return text.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;");
 };
 
 io.on('connection', (client) =>{
   console.log("client connected...");
 
-  client.on("join", function(data: {frq: string, username: string}){
+
+
+  client.on("join", (data: {frq: string, username: string}) => {
 
     console.log(data);
 
     console.log(`user ${data.username} on frequency ${data.frq}`);
 
     client.join(data.frq);
-    client.emit('update', (data.username+" has joined the server on the frequency "+data.frq) );
-    client.emit('host', os.hostname());
+
+    client.to(data.frq).emit('frqUpdate', getFrqStats(data.frq));
+
+    io.emit('statsUpdate', getServerStats());
 
   });
 
 
-  client.on("leave", function(data: {frq: string}){
+  client.on("leave", (data: {frq: string}) => {
 
     console.log('leave', data);
     client.leave(data.frq);
 
+
+    client.to(data.frq).emit('frqUpdate', getFrqStats(data.frq));
+
+    io.emit('statsUpdate', getServerStats());
+
   });
 
 
-  client.on("send", function(data){
+  client.on("send", (data) => {
 
     console.log(data);
 
     if(!data["msg"] || data["msg"]==="" || !data["frq"] || data["frq"]==="") return;
 
     //sanitize data
-    data["frq"]= escape_html(data["frq"]).substring(0, 32);
-    data["msg"]= escape_html(data["msg"]).substring(0, 512);
+    data["frq"]= escapeHtml(data["frq"]).substring(0, 32);
+    data["msg"]= escapeHtml(data["msg"]).substring(0, 512);
 
     client.to(data.frq).emit('chat', data);
 
   });
 
+  client.on('disconnect', () => {
+
+    // TODO: optimize
+
+    Object.keys(io.sockets.adapter.rooms).map( frq => {
+      io.to(frq).emit('frqUpdate', {
+        frq: frq,
+        clientsCount: Object.keys(io.sockets.adapter.rooms[frq].sockets).length
+      });
+
+    });
+  });
+
+  client.emit('host', os.hostname());
+
 });
+
+
+function getServerStats() {
+  return {
+    clientsCount: io.engine.clientsCount,
+    roomsCount: Object.keys(io.sockets.adapter.rooms).length - (io.engine.clientsCount)
+  };
+}
+
+function getFrqStats(frq: string){
+
+  return {
+    frq: frq,
+    clientsCount:  io.sockets.adapter.rooms[frq] ? io.sockets.adapter.rooms[frq].length : 0
+  };
+}
